eec40b4b3992f5f7d2f32cec7419c1f750bc0ed3
nekumiyama <nekumiyama@gmail.com>
2020-09-06 16:35:54 +0900

    fix typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,67 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+//import gray from "gray-percentage"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
+const typography = new Typography({
+  title: 'amaNekublog theme',
+  baseFontSize: '16px',
+  baseLineHeight: 1.70,
+  scaleRatio: 2.10,
+  googleFonts: [
+    {
+      name: 'M+PLUS+Rounded+1c',
+      styles: ['400'],
     },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+    {
+      name: 'Noto+Sans+JP',
+      styles: ['400'],
+    },
+  ],
+  headerFontFamily: [
+    "M PLUS Rounded 1c",
+    "sans-serif",
+  ],
+  bodyFontFamily: [
+    "Noto Sans JP",
+    "sans-serif",
+  ],
+  headerColor: "hsl(210,50%,20%)",
+  bodyColor: "hsl(210,50%,25%)",
+  headerWeight: `bold`,
+  bodyWeight: `normal`,
+  boldWeight: `bold`,
+  blockMarginBottom: 1,
+  includeNormalize: true,
+  overrideStyles: ({ scale, rhythm }, options, styles) => ({
+    'h1': {
+      letterSpacing: '0.08em',
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      lineHeight: 1.20,
+      marginBottom: '1.0rem',
+    },
+    p: {
+      textAlign: 'justify',
+      wordBreak: 'break-all',
+    },
+    a: {
+      textDecoration: "none",
+      color: "#0073bc",
+    },
+    "a:hover": {
+      textDecoration: "underline",
+      color: "#33CCFF",
+    },
+    'li': {
+      marginBottom: '0px',
+    },
+    hr: {
+      marginBottom: '1.0rem'
+    },
+    "pre[class*='language-']": {
+      margin: '0 0 1.7rem !important',
+    },
+  })
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
